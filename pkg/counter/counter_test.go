33e18958e6212d3359722dc15d8df5b5657d4448
nikolay <n.smolov@yandex.ru>
2025-01-04 19:36:51 +0300

    adds counter tests

--- pkg/counter/counter_test.go
+++ pkg/counter/counter_test.go
@@ -1,7 +1,44 @@
 package counter
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCounter(t *testing.T) {
-	t.SkipNow()
+	type test struct {
+		name string
+		arg  int
+		want int
+	}
+
+	tests := []test{
+		{
+			name: "Init is zero number",
+			arg:  0,
+			want: 3,
+		},
+		{
+			name: "Init is negative number",
+			arg:  -10,
+			want: -7,
+		},
+		{
+			name: "Init is positive number",
+			arg:  10,
+			want: 13,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			count := New(test.arg)
+			var current int
+			for range 3 {
+				current = count()
+			}
+			assert.Equal(t, test.want, current)
+		})
+	}
 }
