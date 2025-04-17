619908ec2297eb9dcbe9f520ec2924cb3a90d342
nikolay <n.smolov@yandex.ru>
2025-04-17 14:30:57 +0300

    adds godoc comments to di package

--- pkg/di/di.go
+++ pkg/di/di.go
@@ -1,3 +1,4 @@
+// Package di provides interfaces for project dependency injection.
 package di
 
 import (
@@ -9,36 +10,45 @@ import (
 	"github.com/niksmo/runlytics/pkg/metrics"
 )
 
+// GaugeMetricsGetter is the interface that wraps the GetGaugeMetrics method.
 type GaugeMetricsGetter interface {
 	GetGaugeMetrics() map[string]float64
 }
 
+// CounterMetricsGetter is the interface that wraps the GetCounterMetrics method.
 type CounterMetricsGetter interface {
 	GetCounterMetrics() map[string]int64
 }
 
+// Runner is the interface that wraps the Run method.
 type Runner interface {
 	Run()
 }
 
+// MetricsCollector is the interface that groups
+// the GetGaugeMetrics, GetCounterMetrics and Run methods.
 type MetricsCollector interface {
 	Runner
 	GaugeMetricsGetter
 	CounterMetricsGetter
 }
 
+// Looger is the interface that wraps the Debugw, Infow and Errorw methods.
 type Logger interface {
 	Debugw(msg string, keysAndValues ...any)
 	Infow(msg string, keysAndValues ...any)
 	Errorw(msg string, keysAndValues ...any)
 }
 
+// ServerConfig is the interface that wraps the IsDatabase, SaveInterval and Restore methods.
 type ServerConfig interface {
 	IsDatabase() bool
 	SaveInterval() time.Duration
 	Restore() bool
 }
 
+// FileOperator is the interface that wraps the
+// Clear, Load, Save and Close methods.
 type FileOperator interface {
 	Clear() (err error)
 	Load() ([]byte, error)
@@ -46,26 +56,37 @@ type FileOperator interface {
 	Close() error
 }
 
+// UpdateByNameRepository is the interface that wraps the
+// UpdateCounterByName and UpdateGaugeByName methods.
 type UpdateByNameRepository interface {
 	UpdateCounterByName(ctx context.Context, name string, value int64) (int64, error)
 	UpdateGaugeByName(ctx context.Context, name string, value float64) (float64, error)
 }
 
+// BatchUpdate is the interface that wraps the
+// UpdateCounterList and UpdateGaugeList methods.
 type BatchUpdate interface {
 	UpdateCounterList(ctx context.Context, slice metrics.MetricsList) error
 	UpdateGaugeList(ctx context.Context, slice metrics.MetricsList) error
 }
 
+// ReadByNameRepository is the interface that wraps the
+// ReadCounterByName and ReadGaugeByName methods.
 type ReadByNameRepository interface {
 	ReadCounterByName(ctx context.Context, name string) (int64, error)
 	ReadGaugeByName(ctx context.Context, name string) (float64, error)
 }
 
+// ReadListRepository is the interface that wraps the
+// ReadGauge and ReadCounter methods.
 type ReadListRepository interface {
 	ReadGauge(context.Context) (map[string]float64, error)
 	ReadCounter(context.Context) (map[string]int64, error)
 }
 
+// Repository is the interface that groups
+// the UpdateCounterByName, UpdateGaugeByName, UpdateCounterList, UpdateGaugeList,
+// ReadCounterByName, ReadGaugeByName, ReadGauge, ReadCounter and Run methods.
 type Repository interface {
 	ReadByNameRepository
 	ReadListRepository
@@ -74,32 +95,54 @@ type Repository interface {
 	Run(stopCtx context.Context, wg *sync.WaitGroup)
 }
 
+// HealthCheckService is the interface that wraps the Check method.
 type HealthCheckService interface {
 	Check(ctx context.Context) error
 }
 
+// HTMLService is the interface that wraps the RenderMetricsList method.
 type HTMLService interface {
 	RenderMetricsList(ctx context.Context, buf *bytes.Buffer) error
 }
 
+// ReadService is the interface that wraps the Read method.
 type ReadService interface {
 	Read(context.Context, *metrics.Metrics) error
 }
 
+// UpdateService is the interface that wraps the Update method.
 type UpdateService interface {
 	Update(context.Context, *metrics.Metrics) error
 }
 
+// BatchUpdateService is the interface that wraps the BatchUpdate method.
 type BatchUpdateService interface {
 	BatchUpdate(context.Context, metrics.MetricsList) error
 }
 
-type Job interface {
+// IID is the interface that wraps the ID method.
+type IID interface {
 	ID() int64
+}
+
+// IPayload is the interface that wraps the Payload method.
+type IPayload interface {
 	Payload() []metrics.Metrics
 }
 
-type JobErr interface {
-	ID() int64
+// IErr is the interface that wraps the Err method.
+type IErr interface {
 	Err() error
+}
+
+// Job is the interface that wraps the ID and Payload methods.
+type Job interface {
+	IID
+	IPayload
+}
+
+// JobErr is the interface that wraps the ID and Err methods.
+type JobErr interface {
+	IID
+	IErr
 }
